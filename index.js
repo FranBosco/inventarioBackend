4b43e2c4341cd447edf4b63acafc848f7481fb68
Cristian Larrauri <cristianlarrauri1@gmail.com>
2023-01-31 14:16:41 -0300

    Json

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const server = require('./src/app.js');
-const express = require('express');
-const { conn } = require('./src/db.js');
-const morgan = require('morgan');
-const { preload_insumos } = require('./src/utils/utilsMp');
-const { preload_products } = require('./src/utils/utilsProductos');
+const server = require("./src/app.js");
+const express = require("express");
+const { conn } = require("./src/db.js");
+const morgan = require("morgan");
+const { preload_insumos } = require("./src/utils/utilsMp");
+const { preload_products } = require("./src/utils/utilsProductos");
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-	server.listen(port, () => {
-		preload_insumos();
-		preload_products();
-		console.log(`listening at ${port}`); // eslint-disable-line no-console
-	});
+  server.listen(port, () => {
+    preload_insumos();
+    preload_products();
+    console.log(`listening at ${port}`); // eslint-disable-line no-console
+  });
 });
