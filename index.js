5dd21bfe04559f2ccc5d690b571eff9d05218c47
Cristian Larrauri <cristianlarrauri1@gmail.com>
2023-01-31 14:17:45 -0300

    json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(morgan("dev"));
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     preload_insumos();
     preload_products();
